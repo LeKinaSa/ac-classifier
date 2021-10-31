1eae57e2defc30408c7f97053c1d4ce4d5b300fb
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-10-31 21:16:20 +0000

    Add ROC AUC score and limit max depth to 3

--- python/simple.py
+++ python/simple.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 
 def main():
     train = pd.read_csv('../data/loan_train.csv', sep=';')
@@ -13,12 +14,16 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    dtc = DecisionTreeClassifier(random_state=0)
+    dtc = DecisionTreeClassifier(random_state=0, max_depth=3)
 
     dtc.fit(X_train, y_train)
-    accuracy = dtc.score(X_test, y_test)
+    y_pred = dtc.predict_proba(X_test)[:, 1]
 
+    accuracy = dtc.score(X_test, y_test)
     print(f'Accuracy (%): {round(accuracy * 100, 3)}')
+
+    auc = roc_auc_score(y_test, y_pred)
+    print(f'AUC score: {auc}')
 
     test = test.sort_values(by='loan_id')
 
