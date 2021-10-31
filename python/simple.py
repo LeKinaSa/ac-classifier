7f689cbb7e93553f8cdbf4e4c4d1c93fc203a8f5
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-10-31 20:47:35 +0000

    Add simple DecisionTreeClassifier script

---
+++ python/simple.py
@@ -0,0 +1,35 @@
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+def main():
+    train = pd.read_csv('../data/loan_train.csv', sep=';')
+    test = pd.read_csv('../data/loan_test.csv', sep=';')
+
+    train['status'] = train['status'].apply(lambda x: 0 if x == -1 else x)
+
+    X, y = train.iloc[:, :-1], train.iloc[:, -1]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    dtc = DecisionTreeClassifier(random_state=0)
+
+    dtc.fit(X_train, y_train)
+    accuracy = dtc.score(X_test, y_test)
+
+    print(f'Accuracy (%): {round(accuracy * 100, 3)}')
+
+    test = test.sort_values(by='loan_id')
+
+    X = test.iloc[:, :-1]
+    y = dtc.predict(X)
+
+    submission = X[['loan_id']].copy()
+    submission = submission.rename(columns={'loan_id': 'Id'})
+    submission['Predicted'] = y.tolist()
+
+    submission.to_csv('../data/submissions/simple.csv', index=False)
+
+if __name__ == "__main__":
+    main()
