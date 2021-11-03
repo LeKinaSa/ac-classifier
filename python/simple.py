9681e75675beda2c3c8fa44313695a8907aa8d73
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-11-03 11:36:39 +0000

    Use cross-validation to improve model consistency

--- python/simple.py
+++ python/simple.py
@@ -1,41 +1,43 @@
 
+from data import get_loan_data
+
+import os
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.metrics import roc_auc_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+
+def save_submission(X, y):
+    submission = X[['loan_id']].copy()
+    submission = submission.rename(columns={'loan_id': 'Id'})
+    submission['Predicted'] = y.tolist()
+
+    os.makedirs('../data/submissions/', exist_ok=True)
+    submission.to_csv('../data/submissions/simple.csv', index=False)
 
 def main():
-    train = pd.read_csv('../data/loan_train.csv', sep=';')
-    test = pd.read_csv('../data/loan_test.csv', sep=';')
-
-    train['status'] = train['status'].apply(lambda x: 1 if x == -1 else 0)
+    train, test = get_loan_data()
 
     X, y = train.iloc[:, :-1], train.iloc[:, -1]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+    estimator = GaussianNB()
+    cv = StratifiedKFold()
+    clf = GridSearchCV(estimator, param_grid={}, scoring='roc_auc', cv=cv)
 
-    dtc = DecisionTreeClassifier(random_state=0, max_depth=3)
+    clf.fit(X, y)
 
-    dtc.fit(X_train, y_train)
-    y_pred = dtc.predict_proba(X_test)[:, 1]
-
-    accuracy = dtc.score(X_test, y_test)
-    print(f'Accuracy (%): {round(accuracy * 100, 3)}')
-
-    auc = roc_auc_score(y_test, y_pred)
+    auc = clf.best_score_
     print(f'AUC score: {auc}')
 
     test = test.sort_values(by='loan_id')
 
     X = test.iloc[:, :-1]
-    y = np.round(dtc.predict_proba(X)[:, -1], 5)
+    y = np.round(clf.predict_proba(X)[:, -1], 5)
 
-    submission = X[['loan_id']].copy()
-    submission = submission.rename(columns={'loan_id': 'Id'})
-    submission['Predicted'] = y.tolist()
-
-    submission.to_csv('../data/submissions/simple.csv', index=False)
+    save_submission(X, y)
 
 if __name__ == "__main__":
     main()
