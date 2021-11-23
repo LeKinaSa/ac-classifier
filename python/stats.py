aafa01a39da053defdb704f07ec16a8e30ae7d42
Clara Martins <up201806528@fe.up.pt>
2021-11-23 15:05:14 +0000

    Removed duplicate graphs; Added graphs about Cards, Transactions and Owners

--- python/stats.py
+++ python/stats.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import seaborn
+import seaborn as sb
 import matplotlib.pyplot as plt
 import data
 
@@ -17,7 +17,7 @@ def percentage_plot(df, group, hue):
     df = (df.groupby(group)[hue]
             .value_counts(normalize=True).mul(100)
             .rename('percentage').reset_index())
-    graph = seaborn.catplot(data=df, x=group, y='percentage', kind='bar', 
+    graph = sb.catplot(data=df, x=group, y='percentage', kind='bar', 
             hue=hue, order=order, hue_order=hue_order)
     ax = graph.facet_axis(0, 0)
     for i, p in enumerate(ax.patches):
@@ -29,81 +29,45 @@ def percentage_plot(df, group, hue):
             ha='center', color='black', rotation='horizontal',
         )
     return graph
-
-
-dev, comp = data.get_loan_account_district_data()
-
-def percentage_plot(df, group, hue):
-    group_counts = df.groupby(group)[hue].value_counts()
-    
-    keys = group_counts.to_dict().keys()
-
-    order = remove_dups(list(map(lambda x: x[0], keys)))
-    hue_order = remove_dups(list(map(lambda x: x[1], keys)))
-
-    df = (df.groupby(group)[hue]
-            .value_counts(normalize=True).mul(100)
-            .rename('percentage').reset_index())
-    graph = seaborn.catplot(data=df, x=group, y='percentage', kind='bar', 
-            hue=hue, order=order, hue_order=hue_order)
-    ax = graph.facet_axis(0, 0)
-    for i, p in enumerate(ax.patches):
-        coords = p.get_x() + p.get_width() / 2, p.get_height() + 2
-
-        value = group_counts.iloc[i] # TODO
-        ax.annotate(
-            '{0:.1f}% ({1})'.format(p.get_height(), value), coords,
-            ha='center', color='black', rotation='horizontal',
-        )
-    return graph
-
 
 dev, comp = data.get_loan_account_district_data()
 
 print(dev.nunique())
 print(dev.dtypes)
 
-seaborn.histplot(dev, x="amount")
+sb.histplot(data=dev, x='amount')
 plt.show()
 
-seaborn.histplot(dev, x="status")
+sb.boxplot(data=dev, x='amount')
 plt.show()
 
-seaborn.boxplot(x="district_id", y="amount", data=dev)
+sb.histplot(data=dev, x='status')
 plt.show()
 
-seaborn.boxplot(x="region", y="amount", data=dev)
+sb.boxplot(x='district_id', y='amount', data=dev)
 plt.show()
 
-seaborn.displot(dev, x="region", hue="status", multiple="dodge")
+sb.boxplot(x='region', y='amount', data=dev)
 plt.show()
 
-seaborn.displot(dev, x="frequency", hue="status", multiple="dodge")
+sb.displot(data=dev, x='region', hue='status', multiple='dodge')
 plt.show()
 
-seaborn.displot(dev, x="amount", hue="status", hue_order=[0, 1], multiple="dodge")
+sb.displot(data=dev, x='frequency', hue='status', multiple='dodge')
 plt.show()
 
-seaborn.kdeplot(x="date_x", hue="status", data=dev)
+sb.displot(data=dev, x='amount', hue='status', hue_order=[0, 1], multiple='dodge')
 plt.show()
 
-dev['frequency_percent'] = dev.groupby('status')['frequency'].apply(lambda x: 100*x/x.sum())
+sb.kdeplot(x='date_x', hue='status', data=dev)
+plt.show()
+
+#dev['frequency_percent'] = dev.groupby('status')['frequency'].apply(lambda x: 100*x/x.sum())
 print(len(dev[dev['frequency'] == 'weekly issuance']))
 print(dev.groupby(['frequency', 'status']).sum())
 
 df = dev.groupby('frequency')['status'].value_counts(normalize=True).mul(100).rename('percentage').reset_index()
-seaborn.displot(data=df, x='frequency')
-plt.show()
-
-percentage_plot(dev, 'frequency', 'status')
-plt.show()
-
-dev['frequency_percent'] = dev.groupby('status')['frequency'].apply(lambda x: 100*x/x.sum())
-print(len(dev[dev['frequency'] == 'weekly issuance']))
-print(dev.groupby(['frequency', 'status']).sum())
-
-df = dev.groupby('frequency')['status'].value_counts(normalize=True).mul(100).rename('percentage').reset_index()
-seaborn.displot(data=df, x='frequency')
+sb.displot(data=df, x='frequency')
 plt.show()
 
 percentage_plot(dev, 'frequency', 'status')
@@ -112,30 +76,66 @@ plt.show()
 percentage_plot(dev, 'region', 'status')
 plt.show()
 
-seaborn.displot(data=dev, x='region', hue='status', hue_order=[0,1], multiple='fill')
+sb.displot(data=dev, x='region', hue='status', hue_order=[0,1], multiple='fill')
 plt.show()
 
-seaborn.kdeplot(x="date_x", hue="status", data=comp)
+sb.kdeplot(x='date_x', hue='status', data=comp) # TODO: see
 plt.show()
 
 ### Card ###
 
 card_dev, _ = data.get_card_data()
 
-seaborn.countplot(data=card_dev, x="type", order=["junior", "classic", "gold"])
+sb.countplot(data=card_dev, x='type', order=['junior', 'classic', 'gold'])
+plt.show()
+
+sb.countplot(data=card_dev, x='disp_id')
 plt.show()
 
 ### Transactions ###
 
 trans_dev, _ = data.get_transactions_data()
 
-seaborn.countplot(data=trans_dev, x="type")
+sb.countplot(data=trans_dev, x='type')
 plt.show()
 
-seaborn.countplot(data=trans_dev, x="operation")
+sb.countplot(data=trans_dev, x='operation')
 plt.show()
 
-print("Total transactions:", len(trans_dev))
-print("Transactions without a bank:", trans_dev["bank"].isna().sum())
-seaborn.countplot(data=trans_dev, x="bank")
+print('Total transactions:', len(trans_dev))
+print('Transactions without a bank:', trans_dev['bank'].isna().sum())
+sb.countplot(data=trans_dev, x='bank')
 plt.show()
+
+sb.countplot(data=dev, x='account_id')
+plt.show()
+
+## Owners ###
+owners = data.get_account_owners_data()
+loan_owner_client_dev, loan_owner_client_comp = data.get_loan_client_owner_data()
+
+sb.countplot(data=owners, x='account_id').set(title='Number of Owners per Account')
+plt.show()
+
+sb.countplot(data=owners, x='gender')
+plt.show()
+
+sb.countplot(data=loan_owner_client_dev , x='gender')
+plt.show()
+
+sb.countplot(data=loan_owner_client_comp, x='gender')
+plt.show()
+
+sb.countplot(data=loan_owner_client_dev, x='status', hue='gender')
+plt.show()
+
+loan_owner, _ = data.get_loan_client_owner_data()
+loan_owner['same_district'] = loan_owner['district_id_account'] == loan_owner['district_id_owner']
+print(loan_owner['same_district'].nunique())
+
+loan_owner_districts, _ = data.get_loan_client_owner_district_data()
+loan_owner_districts['same_region'] = loan_owner_districts['region_account'] == loan_owner_districts['region_owner']
+print(loan_owner_districts['same_region'].nunique())
+
+# Há vários loans por conta? Se o cliente já pagou um loan, tem mais probabilidade de pagar outro?
+# 2 novas variáveis: nº de loans por cliente, percentagem de loans pagas
