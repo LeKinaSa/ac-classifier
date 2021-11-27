054fa87561e76688acb77c44bf6547baabb88c7c
Clara Martins <50105043+LeKinaSa@users.noreply.github.com>
2021-11-27 18:17:47 +0000

    Some more correlation graphs

--- python/correlation_analysis.py
+++ python/correlation_analysis.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import data
 
-def correlation_analysis(df):
+def correlation_analysis(df, annot=False, title='Correlation Graph'):
     sb.set_theme(style="white")
 
     # Compute the correlation matrix
@@ -21,31 +21,42 @@ def correlation_analysis(df):
     cmap = sb.diverging_palette(230, 20, as_cmap=True)
 
     # Draw the heatmap with the mask and correct aspect ratio
-    sb.heatmap(corr, mask=mask, cmap=cmap, center=0,
-                square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    sb.heatmap(corr, mask=mask, cmap=cmap, annot=annot, fmt='.01f', center=0,
+                square=True, linewidths=.5, cbar_kws={"shrink": .5}).set(title=title)
     plt.show()
 
-# District
+#### District
 d = data.get_district_data()
 correlation_analysis(d)
 
-# Loan
+#### Loan
 d, _ = data.get_loan_data()
 correlation_analysis(d)
 
-# Loan + Transaction
+#### Loan + Transaction
 t = data.get_improved_transaction_data()
 d = pd.merge(left=d, right=t, on='account_id')
 correlation_analysis(d) # This one is a little slower to show but it has very interesting results
 
-# All
+#### All
 # d, _ = data.get_processed_data()
 # correlation_analysis(d) # This one is too big and it is not good for analyzing
 
-# All Processed
+#### All Processed (Default Processing)
 d, _ = data.get_data()
-d.drop('loan_id', axis=1)
+d = d.drop('loan_id', axis=1)
 correlation_analysis(d)
+correlation_analysis(d, True)
+
+#### All Processed (Default Processing) - Analyzing Correlations By Loan Status
+d, _ = data.get_data()
+d = d.drop('loan_id', axis=1)
+correlation_analysis(d.drop('status', axis=1), True)
+p = d.loc[d['status'] == 0].drop('status', axis=1)
+n = d.loc[d['status'] == 1].drop('status', axis=1)
+correlation_analysis(p, True, 'Correlation Graph (paid loans)')
+correlation_analysis(n, True, 'Correlation Graph (non paid loans)')
+
 
 # TODO: all data with different types of processing in the data
 
