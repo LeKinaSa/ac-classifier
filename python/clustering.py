ab9c1f6154d6141537618afc7129ca45b9565984
Clara Martins <50105043+LeKinaSa@users.noreply.github.com>
2021-12-20 09:20:36 +0000

    Trying clustering to different columns

--- python/clustering.py
+++ python/clustering.py
@@ -1,60 +1,46 @@
 
 from sklearn.cluster import DBSCAN, KMeans, MiniBatchKMeans
 
+import seaborn as sb
+import matplotlib.pyplot as plt
+
 import data
 from data import set_working_directory
-import stats
-
-def analysis(d):
-    stats.scatter_plot(d, 'balance_distribution_median', 'balance_deviation', 'cluster')
-    return
 
 def main():
     d, c = data.get_data()
-    d = d.append(c)
-    to_cluster = [
-        'gender_owner',
-        #'muni_under499_account',
-        #'muni_500_1999_account',
-        #'muni_2000_9999_account',
-        #'muni_over10000_account',
-        'n_cities_account',
-        'ratio_urban_account',
-        'avg_salary_account',
-        'unemployment_95_account',
-        'unemployment_evolution_account',
-        'entrepreneurs_per_1000_account',
-        'crimes_95_per_1000_account',
-        'crimes_evolution_account',
-        'avg_balance',
-        'avg_daily_balance',
-        'balance_deviation',
-        'balance_distribution_first_quarter',
-        'balance_distribution_median',
-        'balance_distribution_third_quarter',
-        'high_balance',
-        'last_high',
-        'last_neg',
-        'negative_balance',
-        'avg_amount',
-        'avg_abs_amount',
-        'card',
-        #'region_non_paid_partial_account'
-    ]
-    fit_clustering = data.select(d, to_cluster)
+    d = d.append(c).drop('frequency', axis=1)
+    
+    clustering_groups = {
+        'client': ['gender_owner', 'age_owner'],
+        'account': ['card', 'disponent'],
+        'district': ['ratio_urban_account', 'avg_salary_account', 'crimes_95_per_1000_account', 'unemployment_95_account', 'entrepreneurs_per_1000_account'],
+        'balance': ['avg_daily_balance', 'balance_deviation', 'high_balance', 'negative_balance'],
+        'all': None,
+    }
     clustering_techniques = {
-        # 'DBSCAN': DBSCAN(eps=5, min_samples=5),
+        #'DBSCAN': DBSCAN(eps=5, min_samples=5),
         'Kmeans': KMeans(n_clusters=5, random_state=0),
         #'MiniBatches': MiniBatchKMeans(n_clusters=8, random_state=0, batch_size=200)
     }
 
-    for technique in clustering_techniques:
-        clustering_technique = clustering_techniques[technique]
-        clustering_technique.fit(fit_clustering)
-        labels = clustering_technique.labels_
-        d['cluster'] = labels
-        analysis(d)
-        
+    for group in clustering_groups:
+        to_cluster = clustering_groups[group]
+        fit_clustering = d
+        if to_cluster != None:
+            fit_clustering = data.select(d, to_cluster)
+        for technique in clustering_techniques:
+            clustering_technique = clustering_techniques[technique]
+            cluster = f'{group} - {technique}'
+            print(cluster)
+            
+            # Apply Clustering
+            clustering_technique.fit(fit_clustering)
+            labels = clustering_technique.labels_
+            d['cluster'] = labels
+
+            # Show Results of Clustering
+            #print(cluster)
     return
 
 if __name__ == '__main__':
